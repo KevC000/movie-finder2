1f6995640cb35b60ff3cdb24bfbac8ce7928c8a1
KevC000 <chenkevin1119@gmail.com>
2023-03-08 16:30:05 -0500

    404

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const NotFound = () => {
 
 const App = () => {
   return (
-    <Router basename="/first-react-app">
+    <Router basename="/movie-finder2">
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <Link className="navbar-brand" to="/">Movie Finder</Link>
       </nav>
