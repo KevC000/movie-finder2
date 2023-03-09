6c05de651c0e09326941136f01d2a390f650bf34
KevC000 <chenkevin1119@gmail.com>
2023-03-09 12:07:40 -0500

    fixed movie id

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Home from './Home';
-
+import Movie from './Movie';
 import './App.css';
 
 const NotFound = () => {
@@ -10,12 +10,13 @@ const NotFound = () => {
 
 const App = () => {
   return (
-    <Router basename="/movie-finder2">
+    <Router>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <Link className="navbar-brand" to="/">Movie Finder</Link>
       </nav>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/movie/:id" component={Movie} />
         <Route component={NotFound} />
       </Switch>
     </Router>
--- src/Movie.js
+++ src/Movie.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import React from 'react';
 import { json, checkStatus } from './utils';
 
 class Movie extends React.Component {
